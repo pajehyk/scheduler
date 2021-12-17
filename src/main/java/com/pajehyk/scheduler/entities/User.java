67f7c4130cc4e507dffe33a424ff659e5a784614
Kirill Popov <pajehyk@gmail.com>
2021-12-18 00:55:46 +0300

    Added User entity

---
+++ src/main/java/com/pajehyk/scheduler/entities/User.java
@@ -0,0 +1,19 @@
+package com.pajehyk.scheduler.entities;
+
+public class User {
+    private String id;
+    private String name;
+
+    public User(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
