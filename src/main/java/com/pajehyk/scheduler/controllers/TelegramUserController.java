9deb53aed19bcd1ef2b2d0122b1bc1ebf15b9220
Kirill Popov <pajehyk@gmail.com>
2022-02-08 15:41:31 +0300

    Added method for changing TelegramUser status in database

--- src/main/java/com/pajehyk/scheduler/controllers/TelegramUserController.java
+++ src/main/java/com/pajehyk/scheduler/controllers/TelegramUserController.java
@@ -1,11 +1,16 @@
 package com.pajehyk.scheduler.controllers;
 
+import com.pajehyk.scheduler.Status;
 import com.pajehyk.scheduler.entities.TelegramUser;
 import com.pajehyk.scheduler.repositories.TelegramUserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class TelegramUserController {
@@ -15,5 +20,11 @@ public class TelegramUserController {
     @GetMapping("/start") 
     public void addTelegramUser(TelegramUser telegramUser){
         telegramUserRepository.save(telegramUser);
+    }
+
+    public void changeTelegramUserStatus(TelegramUser telegramUser, Status status) {
+        Example<TelegramUser> example = Example.of(telegramUser);
+        TelegramUser foundUser = telegramUserRepository.findOne(example).get();
+        foundUser.setStatus(status);
     }
 }
