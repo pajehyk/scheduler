beed5ea1b9b67b33097056c0eb623e625a481e94
Kirill Popov <pajehyk@gmail.com>
2022-02-08 15:40:43 +0300

    Added method for saving task to database

--- src/main/java/com/pajehyk/scheduler/controllers/TaskController.java
+++ src/main/java/com/pajehyk/scheduler/controllers/TaskController.java
@@ -1,7 +1,15 @@
 package com.pajehyk.scheduler.controllers;
 
+import com.pajehyk.scheduler.entities.Task;
+import com.pajehyk.scheduler.repositories.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TaskController {
+    @Autowired
+    private TaskRepository taskRepository;
+    public void addTask(Task task) {
+        taskRepository.save(task);
+    }
 }
