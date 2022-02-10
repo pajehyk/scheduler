6b9091d7b3ae971a4e812d6698772f355d2ded36
Kirill Popov <pajehyk@gmail.com>
2022-02-10 11:52:32 +0300

    Updated TaskController class

--- src/main/java/com/pajehyk/scheduler/controllers/TaskController.java
+++ src/main/java/com/pajehyk/scheduler/controllers/TaskController.java
@@ -9,7 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class TaskController {
     @Autowired
     private TaskRepository taskRepository;
-    public void addTask(Task task) {
-        taskRepository.save(task);
+    public Task addTask(Task task) {
+        Task task1 = taskRepository.save(task);
+        return task1;
     }
+
+    public Task fetchTask(Long taskId) {
+        Task fetchedTask = taskRepository.getById(taskId);
+        return fetchedTask;
+    }
+    public void changeTaskName(Long taskId, String taskName) {
+        Task fetchedTask = fetchTask(taskId);
+        fetchedTask.setTaskName(taskName);
+        taskRepository.save(fetchedTask);
+    }
+
 }
