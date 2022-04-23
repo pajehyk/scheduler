a64136ebaa14578521bdf75b65b6cf79f8ecc3d8
Kirill Popov <pajehyk@gmail.com>
2022-04-23 16:08:42 +0300

    Hid application.properties file and added BotProperties

--- src/main/java/com/pajehyk/scheduler/BotProperties.java
+++ src/main/java/com/pajehyk/scheduler/BotProperties.java
@@ -1,7 +1,6 @@
 package com.pajehyk.scheduler;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
